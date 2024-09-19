9e7414c58e90fc8f5d1f7251c94543bbf30a364c
Harshith <maddilaharshith@gmail.com>
2024-09-19 18:48:44 +1000

    updated file

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+// test/app.test.js
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test', () => {
+    it('should return true', () => {
+        const result = true;
+        expect(result).to.be.true;
+    });
+});
